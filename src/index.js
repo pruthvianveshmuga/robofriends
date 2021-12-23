c0de9bfbafeb6ef4f7152dc559453e0bb65b0900
Pruthvi Anvesh <pruthvianveshmuga@github.com>
2021-12-23 11:06:33 +0530

    index and robots data

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "tachyons";
+import { robots } from "./robots";
+import CardList from "./CardList";
 
 ReactDOM.render(
-  <div>
-    <h1>Hello</h1>
-  </div>,
+  <React.StrictMode>
+    <CardList robots={robots} />
+  </React.StrictMode>,
   document.getElementById("root")
 );
