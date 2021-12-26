168fc0ca9668f569ccb42701865fe07819f188f3
Pruthvi Anvesh <pruthvianveshmuga@github.com>
2021-12-26 15:05:10 +0530

    user hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,43 +1,34 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import ErrorBoundary from "../components/ErrorBoundary";
 import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchStr: "",
-    };
-  }
-  componentDidMount() {
+const App = () => {
+  const [robots, setRobots] = useState([]);
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
-      .then((users) => this.setState({ robots: users }));
-  }
-  onSearch = (str) => {
-    this.setState({ searchStr: str });
-  };
-  render() {
-    const { robots, searchStr } = this.state;
-    const filteredRobots = robots.filter((robot) => {
-      return robot.name.toLowerCase().includes(searchStr.toLowerCase());
-    });
-    return (
-      <div className="tc">
-        <h1>RoboFriends</h1>
-        <SearchBox onSearch={this.onSearch} />
-        <Scroll>
-          <ErrorBoundary>
-            <CardList robots={filteredRobots} />
-          </ErrorBoundary>
-        </Scroll>
-      </div>
-    );
-  }
-}
+      .then((users) => setRobots(users));
+  }, []);
+
+  const [searchStr, setSearchStr] = useState("");
+
+  const filteredRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchStr.toLowerCase());
+  });
+  return (
+    <div className="tc">
+      <h1>RoboFriends</h1>
+      <SearchBox onSearch={(str) => setSearchStr(str)} />
+      <Scroll>
+        <ErrorBoundary>
+          <CardList robots={filteredRobots} />
+        </ErrorBoundary>
+      </Scroll>
+    </div>
+  );
+};
 
 export default App;
